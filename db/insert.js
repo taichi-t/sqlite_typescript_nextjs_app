5492ea21fc1f246da65156add8f1c8e028570f33
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2020-06-14 13:27:51 -0700

    define:insert.js select.js

--- db/insert.js
+++ db/insert.js
@@ -1,27 +1,3 @@
-// const sqlite = require('sqlite3').verbose();
-// const db = new sqlite.Database('./mydb.sqlite'); // SQLite の DB ファイル名
-
-// // SQL を同期的に実行する
-// db.serialize(() => {
-//   // テーブルがなければ作成する
-//   db.run(
-//     'CREATE TABLE IF NOT EXISTS Person (id INTEGER PRIMARY KEY, name TEXT, email TEXT), Vehicle (id INTEGER PRIMARY KEY, brand TEXT model TEXT, ownerId INTEGER KEY REFERENCES Person(id))'
-//   );
-
-//   // Prepared Statement でデータを挿入する
-//   const stmt = db.prepare(
-//     'INSERT INTO Person (name, email) VALUES (buruno, buruno@gmail.com)'
-//   );
-//   stmt.run(['Foo', 25]);
-//   stmt.run(['Bar', 39]);
-//   stmt.run(['Baz', 31]);
-
-//   // prepare() で取得した Prepared Statement オブジェクトをクローズする。これをコールしないとエラーになる
-//   stmt.finalize();
-// });
-
-// db.close();
-
 const sqlite = require('sqlite3').verbose();
 const db = new sqlite.Database('example.sqlite'); // SQLite の DB ファイル名
 
